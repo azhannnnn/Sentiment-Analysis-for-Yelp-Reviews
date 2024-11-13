b8c9accbae97004caf492f70d3313a1c10ebb829
azhannnnn <azhanborntowin22@gmail.com>
2024-11-13 11:24:20 +0530

    first

---
+++ sentiment_analysis_app/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from .analysis import analysis as a
+
+def index(request):
+    return render(request, "index.html")
+
+
+def text_analysis(request):
+    if "submit-button" in request.POST:
+        user_input = str(request.POST.get("entered"))
+        
+        #minimum charcter limit BERT
+        if len(user_input) < 3:
+            return render(request, "text_analysis.html", {"error": "Please enter atleast 3 characters!"})
+        
+        result = a.calculating(user_input)
+        sentence = "The sentiment is "
+        return render(request, "text_analysis.html",{"result": result, "sentence": sentence})
+    
+    return render(request, "text_analysis.html")
+
+
+def yelp(request):
+    if "yelp-submit-button" in request.POST:
+        user_input = str(request.POST.get("url"))
+
+        check_1 = "https://www.yelp."
+        check_2 = "www.yelp.ie"
+
+        if user_input[:17] != check_1 or user_input == check_2:
+            return render(request, "yelp.html", {"error": "Invalid URL"})
+        
+        result = a.yelp(user_input)
+        chart, reviews, sentiments = result
+        
+        if request.POST.get("table-option", "") == "on":
+            return render(request, "yelp.html", {"chart": chart, "reviews": reviews, "sentiments": sentiments, "table": " "})
+        
+        return render(request, "yelp.html", {"chart": chart, "reviews": reviews, "sentiments": sentiments, "no_table": " "})
+    
+    return render(request, "yelp.html")
---
+++ static/scripts/script.js
@@ -0,0 +1,18 @@
+document.getElementById("load").style.visibility = "hidden";
+
+
+function load()
+{
+    document.getElementById("submit-button").style.display = "none";
+    document.getElementById("load").style.visibility = "visible";
+    document.getElementById("display").style.visibility = "hidden";
+}
+
+function yelp_load()
+{
+    document.getElementById("submit-button").style.display = "none";
+    document.getElementById("load").style.visibility = "visible";
+    document.getElementById("graph-div").style.display = "none";
+    document.getElementById("yelp-result").style.display = "none";
+    document.getElementById("yelp-result-table").style.display = "none";
+}
